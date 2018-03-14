9a52ba3ee6b1a0c14d10469c73e6094b6afed087
ZhouYzzz <zhouyz9608@gmail.com>
2018-03-14 10:44:26 +0800

    train_prj to server run

--- train_prj.py
+++ train_prj.py
@@ -105,17 +105,6 @@ def model_fn(features, labels, mode):
   prj_labels = slice_concat([labels['sparse{}'.format(i+1)] for i in range(5)], axis=3)
 
   visualize(tf.concat([prj_labels, prj_outputs], axis=2), 'prjs')
-
-  # loss = tf.reduce_mean(
-  #   tf.map_fn(tf.nn.l2_loss, projection_labels - projection_outputs) / (dataset.INFO.PRJ_SPARSE_WIDTH * dataset.INFO.PRJ_HEIGHT))
-  # loss = l2_loss(projection_labels, projection_outputs)
-  # loss = tf.identity(loss, 'loss')
-
-  # base_loss = tf.reduce_mean(
-  #   tf.map_fn(tf.nn.l2_loss, inputs - projection_labels) / (dataset.INFO.PRJ_SPARSE_WIDTH * dataset.INFO.PRJ_HEIGHT))
-  # # base_loss = l2_loss(inputs, projection_labels)
-  # base_loss = tf.identity(base_loss, 'base_loss')
-
   loss = loss + 1e-4 * tf.add_n(
       [tf.nn.l2_loss(v) for v in tf.trainable_variables()])
   loss = tf.identity(loss, 'total_loss')
@@ -128,7 +117,11 @@ def model_fn(features, labels, mode):
     with tf.control_dependencies(update_ops):
       optimizer = tf.train.MomentumOptimizer(learning_rate=FLAGS.learning_rate, momentum=FLAGS.momentum)
       # optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
-      train_op = optimizer.minimize(loss, global_step=tf.train.get_or_create_global_step())
+      grads_and_vars = optimizer.compute_gradients(loss)
+      clipped_grads_and_vars = [(tf.clip_by_norm(grad, 1e-4), var)
+                                for grad, var in grads_and_vars if grad is not None]
+      train_op = optimizer.apply_gradients(clipped_grads_and_vars, global_step=tf.train.get_or_create_global_step())
+      # train_op = optimizer.minimize(loss, global_step=tf.train.get_or_create_global_step())
   else:
     train_op = None
 
@@ -139,14 +132,151 @@ def model_fn(features, labels, mode):
                                     eval_metric_ops={'rrmse_metric': rrmse_metric})
 
 
+# def l2_loss(t):
+#   return tf.reduce_mean(tf.map_fn(tf.nn.l2_loss, t))
+
+
+
+def branch_network_v2(inputs):
+  stddev = 0.001
+  kernel_initializer = tf.contrib.layers.xavier_initializer()  # tf.random_normal_initializer(stddev=stddev)
+  kernel_initializer = tf.random_normal_initializer(stddev=stddev)
+  inputs = tf.layers.conv2d(inputs, 64, (5, 5), padding='same', data_format='channels_first',
+                            kernel_initializer=kernel_initializer, use_bias=True)
+  inputs = tf.nn.leaky_relu(inputs)
+  inputs = tf.layers.conv2d(inputs, 64, (5, 5), padding='same', data_format='channels_first',
+                            kernel_initializer=kernel_initializer, use_bias=True)
+  inputs = tf.nn.leaky_relu(inputs)
+  sc0 = inputs
+  inputs = tf.layers.conv2d(inputs, 64, (5, 5), padding='same', data_format='channels_first',
+                            kernel_initializer=kernel_initializer, use_bias=True)
+  inputs = tf.nn.leaky_relu(inputs)
+  inputs += sc0
+  inputs = tf.layers.conv2d(inputs, 64, (5, 5), padding='same', data_format='channels_first',
+                            kernel_initializer=kernel_initializer, use_bias=True)
+  inputs = tf.nn.leaky_relu(inputs)
+  sc0 = inputs
+  inputs = tf.layers.conv2d(inputs, 64, (5, 5), padding='same', data_format='channels_first',
+                            kernel_initializer=kernel_initializer, use_bias=True)
+  inputs = tf.nn.leaky_relu(inputs)
+  inputs += sc0
+  inputs = tf.layers.conv2d(inputs, 16, (5, 5), padding='same', data_format='channels_first',
+                            kernel_initializer=kernel_initializer, use_bias=True)
+  inputs = tf.nn.leaky_relu(inputs)
+  inputs = tf.layers.conv2d(inputs, 1, (5, 5), padding='same', data_format='channels_first',
+                            kernel_initializer=kernel_initializer, use_bias=True)
+  inputs = tf.nn.leaky_relu(inputs)
+  inputs = tf.identity(inputs, 'outputs')
+  return inputs
+
+
+def prj_est_network_v2(inputs):
+  branch_outputs = list()
+  for i in range(5):
+    with tf.variable_scope('Branch{}'.format(i)):
+      outputs = branch_network_v2(inputs)
+      branch_outputs.append(outputs)
+  outputs = slice_concat(branch_outputs, axis=3)
+  outputs = tf.identity(outputs, 'outputs')
+  return outputs
+
+
+def prj_model_fn(features, labels, mode):
+  inputs = features['inputs']
+  sparse_outputs = slice_concat([inputs for _ in range(5)], axis=3)
+  # prj_outputs = branch_network_v2(inputs)
+  prj_outputs = prj_est_network_v2(inputs)
+  prj_labels = slice_concat([labels['sparse{}'.format(i+1)] for i in range(5)], axis=3)
+
+  loss = tf.reduce_mean(tf.map_fn(tf.nn.l2_loss, (prj_outputs - prj_labels)))
+  loss = loss / (dataset.INFO.PRJ_DEPTH * dataset.INFO.PRJ_WIDTH * dataset.INFO.PRJ_HEIGHT)
+  loss = tf.identity(loss, 'prj_loss')
+
+  loss += 1e-4 * tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()])
+  loss = tf.identity(loss, 'total_loss')
+
+  base_loss = tf.reduce_mean(tf.map_fn(tf.nn.l2_loss, (sparse_outputs - prj_labels)))
+  base_loss = base_loss / (dataset.INFO.PRJ_DEPTH * dataset.INFO.PRJ_WIDTH * dataset.INFO.PRJ_HEIGHT)
+  base_loss = tf.identity(base_loss, 'base_loss')
+
+  # visualize(prj_outputs, 'prj_outputs')
+  visualize(tf.concat([prj_labels, prj_outputs], axis=2), 'prjs_compare')
+  rrmse_metric = create_rrmse_metric(prj_outputs, prj_labels)
+  tf.identity(rrmse_metric[1], 'rrmse')
+  tf.summary.scalar('rrmse', rrmse_metric[1])
+
+  base_rrmse_metric = create_rrmse_metric(sparse_outputs, prj_labels)
+  tf.identity(base_rrmse_metric[1], 'base_rrmse')
+  tf.summary.scalar('base_rrmse', base_rrmse_metric[1])
+
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+      # optimizer = tf.train.MomentumOptimizer(learning_rate=FLAGS.learning_rate, momentum=FLAGS.momentum)
+      optimizer = tf.train.AdamOptimizer(learning_rate=1e-4)  # FLAGS.learning_rate)
+      grads_and_vars = optimizer.compute_gradients(loss)
+      clipped_grads_and_vars = [(tf.clip_by_norm(grad, 1e-4), var)
+                                for grad, var in grads_and_vars if grad is not None]
+      train_op = optimizer.apply_gradients(clipped_grads_and_vars, global_step=tf.train.get_or_create_global_step())
+
+      # train_op = optimizer.minimize(loss, global_step=tf.train.get_or_create_global_step())
+  else:
+    train_op = None
+  return tf.estimator.EstimatorSpec(mode=mode,
+                                    predictions=prj_outputs,
+                                    loss=loss,
+                                    train_op=train_op)
+
+
+def branch_model_fn(features, labels, mode):
+  inputs = features['inputs']
+  prj_outputs = branch_network_v2(inputs)
+
+  loss = tf.reduce_mean(tf.map_fn(tf.nn.l2_loss, (prj_outputs - labels['sparse1'])))
+  loss = loss / (dataset.INFO.PRJ_DEPTH * dataset.INFO.PRJ_SPARSE_WIDTH * dataset.INFO.PRJ_HEIGHT)
+  loss = tf.identity(loss, 'prj_loss')
+
+  loss += 1e-4 * tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()])
+  loss = tf.identity(loss, 'total_loss')
+
+  base_loss = tf.reduce_mean(tf.map_fn(tf.nn.l2_loss, (inputs - labels['sparse1'])))
+  base_loss = base_loss / (dataset.INFO.PRJ_DEPTH * dataset.INFO.PRJ_SPARSE_WIDTH * dataset.INFO.PRJ_HEIGHT)
+  base_loss = tf.identity(base_loss, 'base_loss')
+
+  visualize(prj_outputs, 'prj_outputs')
+  visualize(tf.concat([labels['sparse1'], prj_outputs], axis=3), 'prjs')
+  rrmse_metric = create_rrmse_metric(prj_outputs, labels['sparse1'])
+  tf.identity(rrmse_metric[1], 'rrmse')
+
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+      # optimizer = tf.train.MomentumOptimizer(learning_rate=FLAGS.learning_rate, momentum=FLAGS.momentum)
+      optimizer = tf.train.AdamOptimizer(learning_rate=1e-4)#FLAGS.learning_rate)
+      grads_and_vars = optimizer.compute_gradients(loss)
+      clipped_grads_and_vars = [(tf.clip_by_norm(grad, 1e-4), var)
+                                for grad, var in grads_and_vars if grad is not None]
+      train_op = optimizer.apply_gradients(clipped_grads_and_vars, global_step=tf.train.get_or_create_global_step())
+
+      # train_op = optimizer.minimize(loss, global_step=tf.train.get_or_create_global_step())
+  else:
+    train_op = None
+  return tf.estimator.EstimatorSpec(mode=mode,
+                                    predictions=prj_outputs,
+                                    loss=loss,
+                                    train_op=train_op)
+
+
+
 def main(_):
   os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpus
   config = tf.estimator.RunConfig().replace(save_checkpoints_secs=1e9,
                                             keep_checkpoint_max=1)
-  tensors_to_log = ['prj_loss', 'total_loss', 'rrmse']
+  tensors_to_log = ['prj_loss', 'total_loss', 'rrmse', 'base_rrmse', 'base_loss']
   logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=100)
   # estimator = tf.estimator.Estimator(model_fn, model_dir=FLAGS.model_dir, config=config)
-  estimator = tf.estimator.Estimator(model_fn, model_dir=None, config=config)
+  # estimator = tf.estimator.Estimator(model_fn, model_dir=None, config=config)
+  estimator = tf.estimator.Estimator(prj_model_fn, model_dir=None, config=config)
 
   estimator.train(lambda: input_fn('train', batch_size=1, num_epochs=1), hooks=[logging_hook], max_steps=2000)
   for _ in range(3):

0cf857c3b98520ac8699c42c125d2062a8f1710b
zhouyzzz <zhouyz9608@gmail.com>
2018-03-26 20:41:12 +0800

    train_rfn.py : add wgan codes

--- train_rfn.py
+++ train_rfn.py
@@ -7,6 +7,7 @@ from model.image_refinement_network import image_refinement_network
 from model.red_cnn import red_cnn
 from utils.summary import visualize
 from utils.rrmse import create_rrmse_metric
+from resnet.cifar10_main import Cifar10Model
 import dataset
 
 import os
@@ -22,10 +23,48 @@ tf.flags.DEFINE_integer('num_epochs', 10, '')
 
 tf.flags.DEFINE_string('gpus', '0', '')
 tf.flags.DEFINE_integer('verbose', False, '')
+
+tf.flags.DEFINE_integer('use_gan', False, '')
+
 FLAGS = tf.flags.FLAGS
 
+def random_crop(images, size, num_crops=5):
+  return [tf.map_fn(lambda i: tf.random_crop(i, size=size), images) for _ in range(num_crops)]
 
-def rfn_model_fn(features, labels, mode):
+def discriminator(inputs, reuse=tf.AUTO_REUSE):
+  # network = ImagenetModel(resnet_size=18, data_format='channels_first', num_classes=1)
+  inputs = tf.transpose(inputs, perm=[0, 2, 3, 1])
+  with tf.variable_scope('Discriminator', reuse=reuse):
+    network = Cifar10Model(resnet_size=20, data_format='channels_first', num_classes=1)
+    inputs = network(inputs, training=True)
+  return inputs
+
+def create_gan_loss_and_train_op(image_outputs, image_labels):
+  samples_crops_fake = random_crop(image_outputs, size=(1, 100, 100))
+  samples_crops_real = random_crop(image_labels, size=(1, 100, 100))
+  ds_fake = [discriminator(s, reuse=tf.AUTO_REUSE) for s in samples_crops_fake]
+  ds_real = [discriminator(s, reuse=tf.AUTO_REUSE) for s in samples_crops_real]
+  d_fake = tf.add_n(ds_fake)
+  d_real = tf.add_n(ds_real)
+  g_loss = tf.reduce_mean(d_fake)
+  d_loss = tf.reduce_mean(d_real - d_fake)
+  reg_loss = FLAGS.weight_decay * tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables(scope='Discriminator')])
+  g_loss += reg_loss
+  d_loss += reg_loss
+  d_vars = tf.trainable_variables(scope='Discriminator')
+  g_vars = tf.trainable_variables(scope='RFN')
+  with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope='Discriminator')):
+      d_rmsprop = tf.train.RMSPropOptimizer(learning_rate=5e-5)\
+          .minimize(d_loss, var_list=d_vars)
+      g_rmsprop = tf.train.RMSPropOptimizer(learning_rate=5e-5)\
+          .minimize(g_loss, var_list=g_vars)
+
+  d_clip = [v.assign(tf.clip_by_value(v, -0.01, 0.01)) for v in d_vars]
+  g_loss = tf.identity(g_loss, 'g_loss')
+  d_loss = tf.identity(d_loss, 'd_loss')
+  return g_loss, d_loss, g_rmsprop, d_rmsprop
+
+def rfn_model_fn(features, labels, mode, params):
   inputs = features['prerfn'] * 50
   with tf.variable_scope('RFN'):
     outputs = image_refinement_network(inputs, training=(mode == tf.estimator.ModeKeys.TRAIN))
@@ -51,6 +90,7 @@ def rfn_model_fn(features, labels, mode):
   if mode == tf.estimator.ModeKeys.TRAIN:
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     with tf.control_dependencies(update_ops):
+      #optimizer = tf.train.MomentumOptimizer(learning_rate=FLAGS.learning_rate, momentum=FLAGS.momentum)
       optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
       grads_and_vars = optimizer.compute_gradients(loss)
       clipped_grads_and_vars = [(tf.clip_by_norm(grad, 1e-4), var)
@@ -58,6 +98,11 @@ def rfn_model_fn(features, labels, mode):
       train_op = optimizer.apply_gradients(clipped_grads_and_vars, global_step=tf.train.get_or_create_global_step())
   else:
     train_op = None
+
+  if FLAGS.use_gan:
+    print('USING WGAN...')
+    _, _, g_train_op, d_train_op = create_gan_loss_and_train_op(outputs, image_labels)
+    train_op = tf.group(train_op, g_train_op, d_train_op)
 
   return tf.estimator.EstimatorSpec(mode=mode,
                                     predictions=outputs,
@@ -72,6 +117,7 @@ def main(_):
   os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpus
   config = tf.estimator.RunConfig(save_checkpoints_secs=1e9,
                                   keep_checkpoint_max=5)
+  #hooks = [tf.train.LoggingTensorHook(tensors=['rrmse', 'base_rrmse', 'd_loss', 'g_loss'],every_n_iter=100)]
   hooks = [tf.train.LoggingTensorHook(tensors=['rrmse', 'base_rrmse'],every_n_iter=100)]
   estimator = tf.estimator.Estimator(rfn_model_fn, model_dir=FLAGS.model_dir, config=config)
 
